a0e971dfa9b92b88deed9bc152dc733464cd8ced
cnstandat <mrnguyendung@cnstandat.net>
2020-06-23 18:03:49 +0700

    sync

--- examples/custom-component-props/src/Utils.js
+++ examples/custom-component-props/src/Utils.js
@@ -1,52 +1,52 @@
-import React from "react";
-import namor from "namor";
-import "./index.css";
+import React from 'react'
+import namor from 'namor'
+import './index.css'
 
 const range = len => {
-  const arr = [];
+  const arr = []
   for (let i = 0; i < len; i++) {
-    arr.push(i);
+    arr.push(i)
   }
-  return arr;
-};
+  return arr
+}
 
 const newPerson = () => {
-  const statusChance = Math.random();
+  const statusChance = Math.random()
   return {
-    firstName: namor.generate({ words: 1, numbers: 0 }),
-    lastName: namor.generate({ words: 1, numbers: 0 }),
+    firstName: namor.generate({words: 1, numbers: 0}),
+    lastName: namor.generate({words: 1, numbers: 0}),
     age: Math.floor(Math.random() * 30),
     visits: Math.floor(Math.random() * 100),
     progress: Math.floor(Math.random() * 100),
     status:
       statusChance > 0.66
-        ? "relationship"
-        : statusChance > 0.33 ? "complicated" : "single"
-  };
-};
+        ? 'relationship'
+        : statusChance > 0.33 ? 'complicated' : 'single',
+  }
+}
 
 export function makeData(len = 5553) {
-  return range(len).map(d => {
-    return {
-      ...newPerson(),
-      children: range(10).map(newPerson)
-    };
-  });
+  return range(len).map(d => ({
+    ...newPerson(),
+    children: range(10).map(newPerson),
+  }))
 }
 
 export const Logo = () =>
-  <div style={{ margin: '1rem auto', display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center'}}>
+  (<div style={{
+    margin: '1rem auto', display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center',
+  }}>
     For more examples, visit {''}
-  <br />
+    <br />
     <a href="https://github.com/react-tools/react-table" target="_blank">
       <img
         src="https://github.com/react-tools/media/raw/master/logo-react-table.png"
-        style={{ width: `150px`, margin: ".5em auto .3em" }}
+        style={{width: '150px', margin: '.5em auto .3em'}}
       />
     </a>
-  </div>;
+  </div>)
 
 export const Tips = () =>
-  <div style={{ textAlign: "center" }}>
+  (<div style={{textAlign: 'center'}}>
     <em>Tip: Hold shift when sorting to multi-sort!</em>
-  </div>;
+  </div>)
